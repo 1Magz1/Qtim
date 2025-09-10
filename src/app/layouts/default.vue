4317486e7efdb59c57fa431035f20871bd3dbe7b
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-09-10 18:31:11 +0700

    update default layout

---
+++ src/app/layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import HeaderComponent from '@/widgets/Header'
+  import FooterComponent from '@/widgets/Footer'
+
+  export default {
+    name: 'DefaultLayout',
+    components: { FooterComponent, HeaderComponent }
+  }
+</script>
+
+<template>
+  <HeaderComponent />
+  <main>
+    <slot />
+  </main>
+  <FooterComponent />
+</template>
+
+<style scoped></style>
