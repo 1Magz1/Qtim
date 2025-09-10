308e70d3cb5adead1d3af0944d84badbd455b7a3
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-09-10 22:06:24 +0700

    add pagination widget

---
+++ src/widgets/Pagination/ui/Pagination.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+  export default {
+    name: 'PaginationComponent'
+  }
+</script>
+
+<script setup lang="ts">
+  const props = defineProps({
+    total: {
+      type: Number,
+      required: true
+    },
+    page: {
+      type: Number,
+      default: 1
+    }
+  })
+
+  const emit = defineEmits<{
+    (e: 'update:page', value: number): void
+  }>()
+
+  const handlePageChange = (newPage: number) => {
+    emit('update:page', newPage)
+  }
+
+  const customUi = {
+    list: 'flex items-center gap-2 pagination',
+    first: 'hidden',
+    last: 'hidden',
+    item: 'w-11 h-full justify-center cursor-pointer',
+    prev: 'w-11 h-full justify-center cursor-pointer',
+    next: 'w-11 h-full justify-center cursor-pointer'
+  }
+</script>
+
+<template>
+  <UPagination
+    :page="props.page"
+    :total="props.total"
+    :ui="customUi"
+    @update:page="handlePageChange"
+  />
+</template>
+
+<style lang="scss">
+  .pagination {
+    height: 44px;
+  }
+</style>
