765f440527faf3eb6f35983036ce09619eea727c
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-09-11 17:04:30 +0700

    add svg icons

---
+++ src/shared/assets/icons/RUFlag.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    width="24"
+    height="24"
+  >
+    <defs>
+      <clipPath id="circleMask">
+        <circle cx="256" cy="256" r="256" />
+      </clipPath>
+    </defs>
+
+    <g clip-path="url(#circleMask)">
+      <rect width="512" height="170.7" y="0" fill="#ffffff" />
+      <rect width="512" height="170.7" y="170.7" fill="#0052b4" />
+      <rect width="512" height="170.7" y="341.3" fill="#d80027" />
+    </g>
+    <circle
+      cx="256"
+      cy="256"
+      r="256"
+      fill="none"
+      stroke="#000"
+      stroke-width="16"
+    />
+  </svg>
+</template>
