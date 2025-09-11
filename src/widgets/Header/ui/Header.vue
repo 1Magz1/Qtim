4032a3b066ea620326ab511e2190f4f0e3a1be05
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-09-11 16:24:57 +0700

    layout header component

--- src/widgets/Header/ui/Header.vue
+++ src/widgets/Header/ui/Header.vue
@@ -1,11 +1,35 @@
-<script lang="ts">
-  export default {
-    name: 'HeaderComponent'
-  }
+<script setup lang="ts">
+  import Logo from '../../../shared/assets/Logo.vue'
 </script>
 
 <template>
-  <header>HeaderComponent</header>
+  <header class="flex justify-between items-center header">
+    <Logo />
+
+    <div class="flex wrapper">
+      <div class="flex items-center link">
+        <NuxtLink class="text-xl" to="/articles">Works</NuxtLink>
+        <NuxtLink class="text-xl" to="/articles">About</NuxtLink>
+      </div>
+
+      <div class="flex items-center gap-4">
+        <span>Contact</span>
+        <UButton class="rounded-full text-xl cursor-pointer" size="xl"
+          >Let’s work</UButton
+        >
+      </div>
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .header {
+    padding: 47px 40px 49px 40px;
+  }
+  .link {
+    gap: 58px;
+  }
+  .wrapper {
+    gap: 91px;
+  }
+</style>
